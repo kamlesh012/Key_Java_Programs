cd4d47b8860e756a3a5c9ca28fa1508bf659371f
Kamlesh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-03-25 07:23:08 +0530

    Create usePkg.java

---
+++ usePkg.java
@@ -0,0 +1,10 @@
+import pkg.*;
+public class usePkg{
+    public static void main(String [] args){
+	pkg1 obj=new pkg1(10,10);
+	obj.pkg1(2,3);
+	obj.display();
+	obj.add(4,5);
+	obj.display();
+    }
+}
